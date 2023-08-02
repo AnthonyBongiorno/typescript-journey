45886731fa53166e60dbbb278e4cbccdd85af8aa
“Anthony <bongiornoanthony6@gmail.com>
2023-08-01 21:23:38 -0400

    finished exercises for the day

--- TS Basics/Ts-Basic-exercises.ts
+++ TS Basics/Ts-Basic-exercises.ts
@@ -76,5 +76,7 @@ function calculateDistance(point1: Point, point2: Point): number {
     let xDiff = point2[0] - point1[0];
     let yDiff = point2[1] - point1[1];
 
-    return Math.sqrt(xDiff * xDiff + yDiff + yDiff);
+    return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 }
+
+console.log(calculateDistance(point2, point1));
